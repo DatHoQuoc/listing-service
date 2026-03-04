df44ffa1a0e15a2f3f335f4d7269a292333adf90
DatHoQuoc <dathoquocdn23@gmail.com>
2026-03-04 18:11:36 +0700

    restore

--- src/main/java/com/dathq/swd302/listingservice/ListingServiceApplication.java
+++ src/main/java/com/dathq/swd302/listingservice/ListingServiceApplication.java
@@ -14,13 +14,13 @@ public class ListingServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ListingServiceApplication.class, args);
     }
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOriginPatterns("*");
-            }
-        };
-    }
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**").allowedOriginPatterns("*");
+//            }
+//        };
+//    }
 }
