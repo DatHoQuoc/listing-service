d894f9f70541efb55549f36764295020e772c7fb
DatHoQuoc <dathoquocdn23@gmail.com>
2026-03-11 20:57:29 +0700

    swagger-config

--- src/main/java/com/dathq/swd302/listingservice/config/SwaggerConfig.java
+++ src/main/java/com/dathq/swd302/listingservice/config/SwaggerConfig.java
@@ -1,17 +1,58 @@
 package com.dathq.swd302.listingservice.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.info.Info;
-@Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                contact = @Contact(
+                        name = "Dat Ho",
+                        email = "solomaster0181@gmail.com",
+                        url = "https://datho.com"
+                ),
+                description = "OpenApi documentation for spring security",
+                title = "OpenApi specification - Dat Ho",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://github.com/dat-ho"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://ferticare.herokuapp.com/api/v1"
+                )
+        },
+        security =  {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("My Super App API")
-                        .version("1.0")
-                        .description("Documentation for My Super App"));
-    }
+
 }
+
